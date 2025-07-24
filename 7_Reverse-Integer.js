38fa55a7f06cd5e9c83dbc0cbe6603177e9b9aed
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-07-24 11:42:28 +0900

    Added Solution 7_Reverse-Integer.js

---
+++ 7_Reverse-Integer.js
@@ -0,0 +1,79 @@
+/**
+ *  7. Reverse Integer
+ */
+/*
+
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+
+Example 2:
+
+Input: x = -123
+Output: -321
+
+Example 3:
+
+Input: x = 120
+Output: 21
+
+ 
+
+Constraints:
+
+    -231 <= x <= 231 - 1
+
+
+*/
+
+
+
+// Solution
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverse = function(x) {
+    // set bounds for integer
+    const minInt = -(2**31);
+    const maxInt = 2**31 - 1;
+    const minDigit= minInt % 10;
+    const maxDigit = maxInt % 10;
+
+    let res = 0;
+    // loop to pop last digits and append it to result
+    while(x !== 0) {
+        // take the last digit from x
+        const digit = x % 10;
+        x = (x / 10) | 0;
+        // check out of bounds
+        if(x / 10 > maxInt || x / 10 === maxInt || x % 10 > maxDigit) return 0;
+        if(x / 10 < minInt || x / 10 === minInt || x % 10 < minDigit) return 0;
+        // append digit to result
+        res = res * 10 + digit;
+    }
+    return res;
+};
+
+
+
+/**
+ *  Lesson Learned
+ *  x = (x / 10) | 0;
+ *  dividing by 10 and Bitwise operator (| 0) convert the operand to a 32-bit signed int
+ *  drop any faction.
+ */
+
+
+
+// Test Cases
+console.log(reverse(123), 321);
+console.log(reverse(-123), -321);
+console.log(reverse(120), 21);

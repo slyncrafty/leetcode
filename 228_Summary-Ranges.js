7510deb16d5f7e2a6d09871d96be61a878f09875
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2026-04-03 22:40:25 +0100

    Add Solution 228_Summary-Ranges.js

---
+++ 228_Summary-Ranges.js
@@ -0,0 +1,77 @@
+/**
+ *  228. Summary Ranges
+ */
+/*
+
+Descriptions:
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+    "a->b" if a != b
+    "a" if a == b
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+
+Example 2:
+
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+
+ 
+
+Constraints:
+
+    0 <= nums.length <= 20
+    -231 <= nums[i] <= 231 - 1
+    All the values of nums are unique.
+    nums is sorted in ascending order.
+
+*/
+
+// Solution
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+	const res = [];
+	for (let i = 0; i < nums.length; i++) {
+		let start = nums[i];
+		while (i + 1 < nums.length && nums[i] + 1 === nums[i + 1]) i++;
+		res.push(start === nums[i] ? `${start}` : `${start}->${nums[i]}`);
+	}
+	return res;
+};
+
+/**
+ *  Lesson Learned
+ */
+
+// Test Cases
+const test = (input, expected) => {
+	const actual = summaryRanges(input);
+	if (JSON.stringify(actual) === JSON.stringify(expected))
+		console.log('✅Test Passed');
+	else console.log('❌Test Failed');
+};
+test([0, 1, 2, 4, 5, 7], ['0->2', '4->5', '7']);
+test([0, 2, 3, 4, 6, 8, 9], ['0', '2->4', '6', '8->9']);

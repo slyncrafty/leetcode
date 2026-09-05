eb4c2e2b9ae1093696d7a6eb374c51b8de476dad
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2026-09-05 23:44:03 +0100

    Add Solution 896_Monotonic-Array.js

---
+++ 896_Monotonic-Array.js
@@ -0,0 +1,65 @@
+/**
+ *  896. Monotonic Array
+ */
+/*
+
+Descriptions:
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,3]
+Output: true
+
+Example 2:
+
+Input: nums = [6,5,4,4]
+Output: true
+
+Example 3:
+
+Input: nums = [1,3,2]
+Output: false
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 105
+    -105 <= nums[i] <= 105
+
+*/
+
+// Solution
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const isMonotonic = function (nums) {
+	let isIncreasing = true;
+	let isDecreasing = true;
+
+	for (let i = 1; i < nums.length; i++) {
+		if (nums[i] < nums[i - 1]) {
+			isIncreasing = false;
+		}
+
+		if (nums[i] > nums[i - 1]) {
+			isDecreasing = false;
+		}
+	}
+
+	return isIncreasing || isDecreasing;
+};
+
+/**
+ *  Insight
+ */
+
+// Test Cases
